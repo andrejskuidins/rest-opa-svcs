503af3d9df1db7864596ece0bb7f995454ab8f55
Andrejs Kuidins <andrejs.kuidins@gmail.com>
2025-11-01 14:53:07 +0200

    feature/jira-01-rest-api

---
+++ kind/install.sh
@@ -0,0 +1,16 @@
+cat <<EOF | kind create cluster --config=-
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+  extraPortMappings:
+  - containerPort: 80
+    hostPort: 80
+    protocol: TCP
+  - containerPort: 443
+    hostPort: 443
+    protocol: TCP
+  - containerPort: 8000
+    hostPort: 8000
+    protocol: TCP
+EOF
